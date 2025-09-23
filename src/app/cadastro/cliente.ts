45bfb3d1ed8e73792439318ca3d9e2ad03739405
thiago_osim <thiago_osim@hotmail.com>
2025-09-23 19:12:25 -0300

    Criado a classe cliente com as propriedades do formulário

---
+++ src/app/cadastro/cliente.ts
@@ -0,0 +1,15 @@
+import { v4 as uuid } from 'uuid'
+
+export class cliente {
+  id?: string;
+  nome?: string;
+  cpf?: string;
+  dataNascimento?: string;
+  email?: string;
+
+  static newCliente() {
+    let novoCliente = new cliente();
+    novoCliente.id = uuid();
+    return novoCliente;
+  }
+}
