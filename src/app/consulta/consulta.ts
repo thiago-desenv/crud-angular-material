03748c1c9b7b8d2f3d9d11430544b91f99de2e67
thiago_osim <thiago_osim@hotmail.com>
2025-09-28 21:25:46 -0300

    Criado layout da página de consulta

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatButtonModule
+],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss'
 })
