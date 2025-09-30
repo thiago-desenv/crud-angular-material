9b16fe54b74ad166e74c4afe04d92a05813a4aed
thiago_osim <thiago_osim@hotmail.com>
2025-09-30 08:58:32 -0300

    Adicionado máscara no cpf

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button'
 import { cliente } from './cliente';
 import { ClienteService } from '../cliente';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-cadastro',
@@ -19,7 +20,10 @@ import { ActivatedRoute, Router } from '@angular/router';
     MatInputModule,
     FormsModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    NgxMaskDirective
+  ], providers: [
+    provideNgxMask()
   ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.scss'
